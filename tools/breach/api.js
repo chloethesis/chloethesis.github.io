9456f91eb058e27d0a99222147728171302ecdad
Chloe <156493057+chloethesis@users.noreply.github.com>
2024-01-15 17:51:40 +0700

    Update api.js

--- tools/breach/api.js
+++ tools/breach/api.js
@@ -1,7 +1,7 @@
 function getInputValue() {
         var inputVal = document.getElementById("myInput").value;
         var anuan = inputVal;
-		var url = "https://haveibeenpwned.com/api/breaches?domain=";
+		var url = "https://haveibeenpwned.com/api/breaches?domain";
         var requestOptions = {
           method: 'GET',
           redirect: 'follow'
