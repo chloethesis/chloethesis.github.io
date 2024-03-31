8597e681d422097ef4666c115ed1e64953e1f0d4
Chloe <156493057+chloethesis@users.noreply.github.com>
2024-03-31 07:06:44 +0700

    Create star.js

---
+++ assets/js/star.js
@@ -0,0 +1,12 @@
+{
+  "@context": "https://schema.org/",
+  "@type": "Book",
+  "name": "Chloethesis",
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "5",
+    "ratingCount": "10080",
+    "bestRating": "5",
+    "worstRating": "1"
+  }
+}
