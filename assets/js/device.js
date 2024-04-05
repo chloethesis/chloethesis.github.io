447ca31a34b2db8ef38275921c3de9b20584cbd1
Nyan Maru <156493057+chloethesis@users.noreply.github.com>
2024-04-06 03:48:18 +0700

    Update device.js

--- assets/js/device.js
+++ assets/js/device.js
@@ -1,6 +1,14 @@
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobile) {
-      document.documentElement.classList.add('mobile');
-    } else {
-      document.documentElement.classList.add('non-mobile');
-    }
+  if (isMobile) {
+    document.documentElement.classList.add('mobile');
+  } else {
+    document.documentElement.classList.add('non-mobile');
+  }
+
+  const deviceTypeElement = document.getElementById('device-type');
+
+  if (isMobile) {
+    deviceTypeElement.textContent = 'mobile';
+  } else {
+    deviceTypeElement.textContent = 'non-mobile';
+  }
