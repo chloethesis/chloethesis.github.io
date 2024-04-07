8d56d66a06a392f550c3211598f7f264ae652acc
Nyan Maru <156493057+chloethesis@users.noreply.github.com>
2024-04-07 08:33:50 +0700

    Create api.js

---
+++ tools/breach/js/api.js
@@ -0,0 +1,14 @@
+function getInputValue() {
+        var inputVal = document.getElementById("myInput").value;
+        var anuan = inputVal;
+		var url = "https://haveibeenpwned.com/api/breaches?domain";
+        var requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        };
+        fetch(url + '=' + anuan, requestOptions)
+          .then(response => response.text())
+          .then(result => document.getElementById("demo").innerText =
+            (result))
+          .catch(error => console.log('error', error));  
+}
