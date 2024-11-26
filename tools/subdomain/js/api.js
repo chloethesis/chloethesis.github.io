854b897aab698c8338d0dfefa2b75f2d7ed4de89
𝙡𝙚𝙭𝙪𝙨𝙮𝙖𝙘𝙝𝙩 ✔ <156493057+chloethesis@users.noreply.github.com>
2024-11-26 17:47:12 +0700

    Create api.js

---
+++ tools/subdomain/js/api.js
@@ -0,0 +1,16 @@
+<script>
+      function getInputValue() {
+        var inputVal = document.getElementById("myInput").value;
+        var anuan = inputVal;
+		var url = "https://api.hackertarget.com/hostsearch/?q";
+        var requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        };
+        fetch(url + '=' + anuan, requestOptions)
+          .then(response => response.text())
+          .then(result => document.getElementById("demo").innerText =
+            (result))
+          .catch(error => console.log('error', error));  
+      }
+    </script>
